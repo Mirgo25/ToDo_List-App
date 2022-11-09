7e4cec0c469b7a71f03051dc47dff3f7a3c4130c
Mirgo25 <mirgo2513@gmail.com>
2022-11-09 22:43:37 +0200

    Added get request

--- routes/todo.js
+++ routes/todo.js
@@ -4,9 +4,10 @@ const Todo = require('../models/todo');
 const router = Router();
 
 // Отримання всіх задач
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        
+        const todos = await Todo.findAll();
+        res.status(200).json(todos);
     } catch (e) {
         console.log(e);
         res.status(500).json({
